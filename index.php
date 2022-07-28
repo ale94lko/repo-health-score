4f079df1269c4466b83f0c9d25b48115f8463a2e
Alejandro <47539231+ale94lko@users.noreply.github.com>
2022-07-27 23:06:47 -0400

    Change deploy path and method

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php declare(strict_types=1);
 
 $basePath = realpath(__DIR__);
+$filePath = $basePath . '/dist';
+
+if (!is_dir($filePath)) {
+    umask(000);
+    mkdir($filePath, 0775);
+}
+
 require $basePath . '/vendor/autoload.php';
 require $basePath . '/src/BadgeGenerator.php';
 require $basePath . '/src/Request.php';
@@ -9,6 +16,6 @@ $request = new Request(getenv('REPOSITORY'));
 $healthPercentage = $request->getHealthPercentage();
 
 $image = (new BadgeGenerator($healthPercentage))->generate();
-$imagePath = $basePath . '/badge.svg';
+$imagePath = $basePath . $filePath . '/badge.svg';
 
 file_put_contents($imagePath, $image);
